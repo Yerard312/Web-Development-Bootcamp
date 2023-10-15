3cb0a980ff8da890b7a3bf597dc813c0d137b096
Yerard312 <yerard_555@hotmail.com>
2023-10-15 14:55:46 -0500

    Create Dia1.js

---
+++ Semana1/Dia1.js
@@ -0,0 +1,26 @@
+//Codigo Dia 1 JS
+/*Se realiza la declaracion de variales*/
+
+
+var myName = "Yerard";
+var myAge = 32;
+
+let myCity ="Bogota";
+let myBool = True;
+
+const MY_ID = "1022365525";
+
+console.log (myName);
+console.log (typeof myName);
+
+console.log (myAge);
+console.log (typeof myAge);
+
+console.log (myCity);
+console.log (typeof myCity);
+
+console.log (myBool);
+console.log (typeof myBool);
+
+console.log (MY_ID);
+console.log (typeof MY_ID);
